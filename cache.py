da155f19d7ec910236008d02f40a13bbe6668fce
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-01-27 17:15:21 +0100

    Add file that should regenerate data

---
+++ cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import requests
+from lxml import html
+import shutil
+import os
+
+base = "https://outreachdashboard.wmflabs.org/campaigns/"
+
+campaigns = [
+	'studenti',
+	'seniori',
+	'knihovny',
+	'workshopy',
+]
+
+# Clean data
+if os.path.isdir('public/data'):
+	shutil.rmtree('public/data')
+	os.mkdir('public/data')
+else:
+	os.mkdir('public/data')
+
+for campaign in campaigns:
+	url = base + campaign + '/users'
+	r = requests.get(url)
+	tree = html.fromstring(r.content)
+	users = tree.xpath('//*[@id="users"]/table/tbody/tr/td/a/text()')
+	f = open('public/data/' + campaign + '-users.txt', 'w')
+	for user in users:
+		f.write(user.encode('latin1') + '\n')
+	f.close()
